cd09e1f6eaafb5e3f6a304860c04224953e4d3dd
arishimam <62866346+arishimam@users.noreply.github.com>
2023-05-19 23:18:41 -0700

    Final?

--- Crossword/CrosswordApp.swift
+++ Crossword/CrosswordApp.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 @main
 struct CrosswordApp: App {
+    // Randomly chooses crossword puzzle from this array of strings of JSON puzzle files
+//    let puzzleFiles = ["puzzle", "puzzle2", "puzzle3"]
+
+//    init() {
+//        let randomPuzzleFile = puzzleFiles.randomElement() ?? "puzzle" // Fallback to "puzzle1" if the array is empty
+//        _gameManager = StateObject(wrappedValue: GameManager(puzzleFile: randomPuzzleFile))
+//    }
     @StateObject var gameManager = GameManager(puzzleFile: "puzzle")
 
     var body: some Scene {
